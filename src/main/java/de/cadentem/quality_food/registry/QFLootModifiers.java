bf1c2f80b11f67c59ab5f7d19dde1e3a08bac310
Kai <kai.zahn@yahoo.de>
2024-02-28 10:01:42 +0100

    adjustments - apply quality to chest loot - apply quality to more harvestable items
    (cherry picked from commit edef6e285731b921133591e5974e67d963e064f2)

--- src/main/java/de/cadentem/quality_food/registry/QFLootModifiers.java
+++ src/main/java/de/cadentem/quality_food/registry/QFLootModifiers.java
@@ -2,7 +2,7 @@ package de.cadentem.quality_food.registry;
 
 import com.mojang.serialization.Codec;
 import de.cadentem.quality_food.QualityFood;
-import de.cadentem.quality_food.core.loot_modifiers.BlockLootModifier;
+import de.cadentem.quality_food.core.loot_modifiers.QualityLootModifier;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,5 +11,5 @@ import net.minecraftforge.registries.RegistryObject;
 public class QFLootModifiers {
     public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIERS = DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, QualityFood.MODID);
 
-    public static final RegistryObject<Codec<BlockLootModifier>> BLOCK_LOOT_MODIFIER = LOOT_MODIFIERS.register(BlockLootModifier.ID, () -> BlockLootModifier.CODEC);
+    public static final RegistryObject<Codec<QualityLootModifier>> QUALITY_LOOT_MODIFIER = LOOT_MODIFIERS.register(QualityLootModifier.ID, () -> QualityLootModifier.CODEC);
 }
