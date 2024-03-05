2c8dcd50db303e16d09aa88196d48e79e97df948
Kai <kai.zahn@yahoo.de>
2024-03-05 14:09:33 +0100

    fix tomato and rice quality

---
+++ src/main/java/de/cadentem/quality_food/mixin/farmersdelight/RiceBlockMixin.java
@@ -0,0 +1,23 @@
+package de.cadentem.quality_food.mixin.farmersdelight;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import de.cadentem.quality_food.util.Utils;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import vectorwing.farmersdelight.common.block.RiceBlock;
+
+/** Support for quality block state */
+@Mixin(RiceBlock.class)
+public abstract class RiceBlockMixin {
+    @ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;setBlockAndUpdate(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z"))
+    private BlockState quality_food$keepQualityWhenGrowing(final BlockState original, @Local(argsOnly = true) final BlockState instance) {
+        return original.setValue(Utils.QUALITY_STATE, instance.getValue(Utils.QUALITY_STATE));
+    }
+
+    @ModifyArg(method = "performBonemeal", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"))
+    private BlockState quality_food$keepQualityOnBonemeal(final BlockState original, @Local(argsOnly = true) final BlockState instance) {
+        return original.setValue(Utils.QUALITY_STATE, instance.getValue(Utils.QUALITY_STATE));
+    }
+}
