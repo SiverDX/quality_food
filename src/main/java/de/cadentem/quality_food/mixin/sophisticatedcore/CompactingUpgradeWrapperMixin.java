41f6e61f4b2c6eda96db2dcd5635905a7b34f8c4
Kai <kai.zahn@yahoo.de>
2024-03-10 00:40:56 +0100

    sophisticated core compacting + generally better compacting support
    (cherry picked from commit f82f9fbb885f1c103ac52bc916e487c2bf24c185)

---
+++ src/main/java/de/cadentem/quality_food/mixin/sophisticatedcore/CompactingUpgradeWrapperMixin.java
@@ -0,0 +1,36 @@
+package de.cadentem.quality_food.mixin.sophisticatedcore;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import de.cadentem.quality_food.util.QualityUtils;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.compacting.CompactingUpgradeWrapper;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Debug(export = true)
+@Mixin(value = CompactingUpgradeWrapper.class)
+public abstract class CompactingUpgradeWrapperMixin {
+    @SuppressWarnings("ConstantConditions")
+    @ModifyExpressionValue(method = "compactSlot", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;hasTag()Z"), remap = false)
+    private boolean quality_food$allowQualityTag(boolean hasTag, @Local final ItemStack stack) {
+        if (hasTag) {
+            CompoundTag tag = stack.getTag();
+
+            if (QualityUtils.hasQuality(stack) && tag.getAllKeys().size() == 1) {
+                return false;
+            }
+        }
+
+        return hasTag;
+    }
+
+    @ModifyVariable(method = "tryCompacting", at = @At(value = "STORE"), ordinal = 1, remap = false)
+    private ItemStack quality_food$handleConversion(final ItemStack compacted, @Local(ordinal = 0) final ItemStack ingredient) {
+        QualityUtils.applyQuality(compacted, QualityUtils.getQuality(ingredient));
+        return compacted;
+    }
+}
