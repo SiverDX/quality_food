cc6f102d92f787e3ff053b31e2950b1b63a726a7
Kai <kai.zahn@yahoo.de>
2024-08-13 13:31:42 +0200

    re-enable sophisticated core compat

--- src/main/java/de/cadentem/quality_food/mixin/sophisticatedcore/CompactingUpgradeWrapperMixin.java
+++ src/main/java/de/cadentem/quality_food/mixin/sophisticatedcore/CompactingUpgradeWrapperMixin.java
@@ -1,32 +1,31 @@
 package de.cadentem.quality_food.mixin.sophisticatedcore;
 
-//import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-//import com.llamalad7.mixinextras.sugar.Local;
-//import de.cadentem.quality_food.util.QualityUtils;
-//import net.minecraft.world.item.ItemStack;
-//import net.p3pp3rf1y.sophisticatedcore.upgrades.compacting.CompactingUpgradeWrapper;
-//import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.injection.At;
-//import org.spongepowered.asm.mixin.injection.ModifyVariable;
-//
-//@Mixin(CompactingUpgradeWrapper.class)
-//public abstract class CompactingUpgradeWrapperMixin {
-//    @SuppressWarnings("ConstantConditions")
-//    @ModifyExpressionValue(method = "compactSlot", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;hasTag()Z"))
-//    private boolean quality_food$allowQualityTag(boolean hasTag, @Local final ItemStack stack) {
-//        if (hasTag) {
-//            if (QualityUtils.hasQuality(stack) && stack.getTag().getAllKeys().size() == 1) {
-//                // Ignore quality tag
-//                return false;
-//            }
-//        }
-//
-//        return hasTag;
-//    }
-//
-//    @ModifyVariable(method = "tryCompacting", at = @At(value = "STORE"), ordinal = 1, remap = false)
-//    private ItemStack quality_food$handleConversion(final ItemStack compacted, @Local(ordinal = 0) final ItemStack ingredient) {
-//        QualityUtils.applyQuality(compacted, QualityUtils.getQuality(ingredient));
-//        return compacted;
-//    }
-//}
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import de.cadentem.quality_food.util.QualityUtils;
+import net.minecraft.core.component.DataComponentPatch;
+import net.minecraft.world.item.ItemStack;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.compacting.CompactingUpgradeWrapper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(CompactingUpgradeWrapper.class)
+public abstract class CompactingUpgradeWrapperMixin {
+    /** Allow compacting when only quality data is present */
+    @ModifyExpressionValue(method = "compactSlot", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getComponentsPatch()Lnet/minecraft/core/component/DataComponentPatch;"))
+    private DataComponentPatch quality_food$allowQualityTag(final DataComponentPatch original, @Local final ItemStack stack) {
+        if (original.size() == 1 && QualityUtils.hasQuality(stack)) {
+            return DataComponentPatch.EMPTY;
+        }
+
+        return original;
+    }
+
+    /** Retain quality when compacting */
+    @ModifyVariable(method = "tryCompacting", at = @At(value = "STORE"), ordinal = 1, remap = false)
+    private ItemStack quality_food$handleConversion(final ItemStack compacted, @Local(ordinal = 0) final ItemStack ingredient) {
+        QualityUtils.applyQuality(compacted, QualityUtils.getQuality(ingredient));
+        return compacted;
+    }
+}
