21d8c8d557d00f8954ff57327190a81c134486fe
Kai <kai.zahn@yahoo.de>
2025-10-28 20:17:01 +0100

    untested create fix

--- src/main/java/de/cadentem/quality_food/mixin/create/RecipeApplierMixin.java
+++ src/main/java/de/cadentem/quality_food/mixin/create/RecipeApplierMixin.java
@@ -18,13 +18,13 @@ import java.util.List;
 @Mixin(value = RecipeApplier.class, remap = false)
 public abstract class RecipeApplierMixin {
     /** Used by fans or mechanical press */
-    @Inject(method = "applyRecipeOn(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/crafting/Recipe;)Ljava/util/List;", at = @At(value = "RETURN"))
-    private static void quality_food$applyQuality(final Level level, final ItemStack input, final Recipe<?> recipe, final CallbackInfoReturnable<List<ItemStack>> callback) {
+    @Inject(method = "applyRecipeOn(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/crafting/Recipe;Z)Ljava/util/List;", at = @At(value = "RETURN"))
+    private static void quality_food$applyQuality(final Level level, final ItemStack input, final Recipe<?> recipe, final boolean returnProcessingRemainder, final CallbackInfoReturnable<List<ItemStack>> callback) {
         callback.getReturnValue().forEach(stack -> QualityUtils.applyQuality(stack, List.of(input), null));
     }
 
     /** Used my mechanical press */
-    @ModifyVariable(method = "applyRecipeOn(Lnet/minecraft/world/entity/item/ItemEntity;Lnet/minecraft/world/item/crafting/Recipe;)V", at = @At("STORE"))
+    @ModifyVariable(method = "applyRecipeOn(Lnet/minecraft/world/entity/item/ItemEntity;Lnet/minecraft/world/item/crafting/Recipe;Z)V", at = @At("STORE"))
     private static List<ItemStack> quality_food$applyQuality(final List<ItemStack> result, @Local(argsOnly = true) final ItemEntity ingredient) {
         result.forEach(stack -> QualityUtils.applyQuality(stack, List.of(ingredient.getItem()), null));
         return result;
