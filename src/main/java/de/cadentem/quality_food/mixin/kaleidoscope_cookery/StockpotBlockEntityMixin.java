0fda4acc9a6c15a966fc026bb9eaa993de9646d0
Cadentem <kai.zahn@yahoo.de>
2026-06-13 19:09:48 +0200

    remove remap

--- src/main/java/de/cadentem/quality_food/mixin/kaleidoscope_cookery/StockpotBlockEntityMixin.java
+++ src/main/java/de/cadentem/quality_food/mixin/kaleidoscope_cookery/StockpotBlockEntityMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = StockpotBlockEntity.class, remap = false)
+@Mixin(StockpotBlockEntity.class)
 public abstract class StockpotBlockEntityMixin {
     @Shadow
     private ItemStack result;
