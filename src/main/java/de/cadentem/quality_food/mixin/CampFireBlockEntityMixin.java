e19257f376c269e2c8c48edd229eb72ad0ca6548
Cadentem <kai.zahn@yahoo.de>
2026-08-08 01:12:35 +0200

    fix #96

---
+++ src/main/java/de/cadentem/quality_food/mixin/CampFireBlockEntityMixin.java
@@ -0,0 +1,18 @@
+package de.cadentem.quality_food.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import de.cadentem.quality_food.util.QualityUtils;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.CampfireBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(CampfireBlockEntity.class)
+public abstract class CampFireBlockEntityMixin {
+    @ModifyVariable(method = "cookTick", at = @At(value = "STORE"), ordinal = 1)
+    private static ItemStack quality_food$applyQuality(final ItemStack cookedItem, @Local(ordinal = 0) final ItemStack ingredient) {
+        QualityUtils.applyQuality(cookedItem, QualityUtils.getQuality(ingredient));
+        return cookedItem;
+    }
+}
